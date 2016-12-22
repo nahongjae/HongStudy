69ebf6b702544ea9ba40e352191f87024e4e27b1
nahongjae <skghdwo4@naver.com>
2016-12-22 15:20:34 +0900

    df

--- src/day3/test3.java
+++ src/day3/test3.java
@@ -4,21 +4,26 @@ public class test3 {
 	public static void main(String[] args) {
 		
 	}
-	int a;
-	Integer b;
-	Integer c=0; //Integer c = new Integer(0);
-	
-	void sub1(){
-		int s = a+10;
-		System.out.println(s); //10
-	}
-	void sub2(){
-		int d = b+10; // b는 null
-		System.out.println(d);
-		
-	}
-	void sub3(){
-		int f = c +10;
-		System.out.println(f); //10
+
+	class hamsu {
+		int a;
+		Integer b;
+		Integer c = 0; // Integer c = new Integer(0);
+
+		void sub1() {
+			int s = a + 10;
+			System.out.println(s); // 10
+		}
+
+		void sub2() {
+			int d = b + 10; // b는 null
+			System.out.println(d);
+
+		}
+
+		void sub3() {
+			int f = c + 10;
+			System.out.println(f); // 10
+		}
 	}
 }
