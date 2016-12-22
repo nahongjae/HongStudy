849c0772bc8731f75dbc82597a5fb5939b80d406
nahongjae <skghdwo4@naver.com>
2016-12-22 15:02:08 +0900

    df

--- src/day3/test.java
+++ src/day3/test.java
@@ -4,6 +4,29 @@ public class test {
 	public static void main(String[] args) {
 		User1 oo = new User1();
 		oo.print(5);
+		System.out.println();	
+		
+		int sum = oo.sum(10);
+		System.out.println(sum);
+		
+		int s;
+		s=oo.pow(2, 10);
+		System.out.println(s);
+		
+		byte b= 10;
+		oo.write(b); //short
+		
+		short ss= 10;
+		oo.write(ss); // short
+		
+		Short dd= 10;
+		oo.write(dd); // Short
+		
+		int n= 10;
+		oo.write(n); //int
+		
+		long l =10;
+		oo.write(l); //Long
 	}
 }
 
@@ -17,6 +40,32 @@ class User1 {
 		return n>=1 ? n+sum(n-1):n;
 	}
 	
+	
 	//a의 b pow(2,10) => 1024. 단 
+	public int pow (int a, int b){
+		
+		return b>=1? a*pow(a,b-1):1;
+		
+	}
+	//byte=> byte, char=> Character, int=>Integer
+	public void write(Short a){
+		System.out.println("Short");
+	}
+	public void write(short a){
+		System.out.println("short");
+	}
+	
+	public void write(int a){
+		System.out.println("int");
+	}
+	public void write(Long a){
+		System.out.println("Long");
+	}
+	
+	
 }
+class Demo1{
+	private Demo1(){}
+}
+
 
---
+++ src/day3/test2.java
@@ -0,0 +1,25 @@
+package day3;
+
+public class test2 {
+	
+		int a;
+		Integer b;
+
+		Integer c=0; // Integer c = new Integer(0);
+
+	void sub1() {
+		int s = a + 10;
+		System.out.println(s); //10
+	}
+
+//	void sub2() {
+//		int s = b + 10; // b는 null
+//		System.out.println(s); // error
+//	}
+
+	void sub3() {
+		int s = c + 10;
+		System.out.println(s); // 10
+	}
+
+}
